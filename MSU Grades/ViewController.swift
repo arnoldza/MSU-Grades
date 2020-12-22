7aad48a7b1843f719c508f8b4536266c9e0c6859
Zach Arnold <arnoldza@msu.edu>
2020-12-21 21:46:31 -0500

    Initial Commit

---
+++ MSU Grades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSU Grades
+//
+//  Created by Zach Arnold on 12/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
